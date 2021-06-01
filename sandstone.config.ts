97f232e1afbcc794cd6695a93ffe009c07e18798
0b10000 <81607037+0b10000@users.noreply.github.com>
2021-05-31 23:44:03 -0400

    Initial commit

---
+++ sandstone.config.ts
@@ -0,0 +1,10 @@
+import type { SandstoneConfig } from 'sandstone'
+
+export default {
+  name: 'trains',
+  description: [ 'A ', { text: 'Sandstone', color: 'gold' }, ' data pack.' ],
+  formatVersion: 6,
+  namespace: 'trains',
+  packUid: 'Xj-ME-4g',
+  saveOptions: { path: 'build' }
+} as SandstoneConfig
