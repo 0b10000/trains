1d378be405e7314b219d2c1ea32507a32c3f10d2
0b10000 <zerob10000@tutanota.com>
2022-03-24 15:10:09 -0400

    lint sandstone config

--- sandstone.config.ts
+++ sandstone.config.ts
@@ -1,10 +1,10 @@
-import type { SandstoneConfig } from 'sandstone'
+import type {SandstoneConfig} from 'sandstone';
 
 export default {
-  name: 'trains',
-  description: [ 'A ', { text: 'Sandstone', color: 'gold' }, ' data pack.' ],
-  formatVersion: 6,
-  namespace: 'trains',
-  packUid: 'Xj-ME-4g',
-  saveOptions: { path: 'build' }
-} as SandstoneConfig
+	name: 'trains',
+	description: ['A ', {text: 'Sandstone', color: 'gold'}, ' data pack.'],
+	formatVersion: 6,
+	namespace: 'trains',
+	packUid: 'Xj-ME-4g',
+	saveOptions: {path: 'build'},
+} as SandstoneConfig;
