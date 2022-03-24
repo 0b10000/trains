bc8e2b3845d8a500c6b98ff1b159bbcb5a182c10
0b10000 <zerob10000@tutanota.com>
2022-03-24 13:48:41 -0400

    apply eslint rules

--- src/welcome.ts
+++ src/welcome.ts
@@ -1,20 +1,19 @@
-/**
- * This file is just an example.
- * You can delete it!
- */
-
-import { MCFunction, tellraw } from 'sandstone'
+/* eslint-disable new-cap */
+import {MCFunction, tellraw} from 'sandstone';
 
 MCFunction('welcome_message', () => {
-  tellraw('@a', [
-    '\n========= Congratulations! =========\n\n',
-    { text: ' trains', color: 'gold', bold: true }, ' is ', { text: 'successfully installed.\n\n', color: 'green' },
-    ' Created by 0b10000\n',
-    ' Inspired by flashteens\n',
-    ' Compiled using ', {text: 'Sandstone', color: "gold", bold: true, clickEvent: {action: "open_url", value: "https://sandstone.dev"}} ,
-    '\n\n',
-    '=================================',
-  ])
+	tellraw('@a', [
+		'\n========= Congratulations! =========\n\n',
+		{text: ' trains', color: 'gold', bold: true},
+		' is ',
+		{text: 'successfully installed.\n\n', color: 'green'},
+		' Created by 0b10000\n',
+		' Inspired by flashteens\n',
+		' Built with ',
+		{text: 'Sandstone', color: 'gold', bold: true, clickEvent: {action: 'open_url', value: 'https://sandstone.dev'}},
+		'\n\n',
+		'=================================',
+	]);
 }, {
-  runOnLoad: true,
-})
+	runOnLoad: true,
+});
